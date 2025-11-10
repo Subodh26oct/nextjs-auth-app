e03932543931de5b8c89ad58afc1778abe4ca49c
Subodh26oct <subodh261003kumar@gmail.com>
2025-11-10 22:05:55 +0530

    Fix verification link path to /verifyemail

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/login/verifyemail',
+        destination: '/verifyemail',
+        permanent: true,
+      },
+    ];
+  },
   experimental: {
   },
   reactStrictMode: true,
